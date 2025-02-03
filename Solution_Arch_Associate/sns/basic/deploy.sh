e74faf80b90a2fdeb5bb68ab97fae5c0daf7505e
Mohamed Saleh <mohamedsaleh.elec@gmail.com>
2025-02-03 13:23:59 +0100

    sns basics

---
+++ Solution_Arch_Associate/sns/basic/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Deploy Sns Basic example"
+
+STACK_NAME="SnsBasic"
+
+aws cloudformation deploy \
+--template-file ./template.yaml \
+--stack-name $STACK_NAME \
+--no-execute-changeset \
+--region eu-central-1 \
+--capabilities CAPABILITY_NAMED_IAM
